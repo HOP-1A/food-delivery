097f2cfaefc1e461ff6c756c31e8a5aeaeb8d3b7
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2025-03-18 18:24:40 +0800

    done

--- src/app/_components/HomePageItemComp.tsx
+++ src/app/_components/HomePageItemComp.tsx
@@ -1,41 +1,39 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardTitle,
+  CardDescription,
 } from "@/components/ui/card";
-type ProductDataType = {
-  productCover: string;
-  productName: string;
-  productPrice: string;
-};
 import { ShoppingBag } from "lucide-react";
-export default function HPItemComp(productData: ProductDataType) {
-  return (
-    <div>
-      <Card className="w-1/6 pr-1/12 pl-1/12 pb-1/12 pt-1/12 flex justify-center bg-amber-50 group">
-        <CardContent className="flex justify-center w-full">
-          <img
-            className="w-full"
-            src={productData.productCover}
-            alt={productData.productName}
-          ></img>
-        </CardContent>
+import { Products } from "../page";
 
-        <CardFooter className="flex flex-col mt-1/12">
-          <CardTitle className="text-[24px] text-orange-900 h-max text-center font-black">
-            {productData.productName}
-          </CardTitle>
-          <CardDescription className="text-[24px] text-orange-900 font-black">
-            {productData.productPrice}
-          </CardDescription>
-          <button className="hidden group-hover:flex w-2/3 border-[1px] bg-green-600 md:flex items-center p-[8px] justify-center rounded-md gap-2.5 mt-1.5">
+export default function HPItemComp({ productData }: { productData: Products }) {
+  return (
+    <Card className="w-2/6 pr-1/12 pl-1/12 pb-1/12 pt-1/12 flex justify-center bg-amber-50 group relative overflow-hidden">
+      <CardContent className="flex justify-center w-full relative">
+        <img
+          className="w-full transition-opacity duration-300 group-hover:opacity-30"
+          src={productData.imgUrl}
+          alt={productData.name}
+        />
+
+        <button className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          <div className="w-2/3 border-[1px] bg-green-600 flex items-center p-[10px] justify-center rounded-md gap-2.5">
             <ShoppingBag />
             <span className="text-sm text-white">Сагсанд хийх</span>
-          </button>
-        </CardFooter>
-      </Card>
-    </div>
+          </div>
+        </button>
+      </CardContent>
+
+      <CardFooter className="flex flex-col mt-1/12 text-center">
+        <CardTitle className="text-[24px] text-orange-900 font-black">
+          {productData.name}
+        </CardTitle>
+        <CardDescription className="text-[24px] text-orange-900 font-black">
+          {productData.price}
+        </CardDescription>
+      </CardFooter>
+    </Card>
   );
 }
