e6ec0cd248860e3aa4aab4739b827b5275d805e5
24HP6298 24HP6298 <24HP6298@admins-iMac.local>
2025-03-20 01:13:09 -0700

    AdminTable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className="bg-[rgb(245,235,220)] bg-opacity-[var(--tw-bg-opacity)] min-h-screen">
       <CategoryComponent />
-      <OrderAddressPage/>
+      <OrderAddressPage />
     </div>
   );
 };
