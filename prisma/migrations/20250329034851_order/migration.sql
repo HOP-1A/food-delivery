95ca607d2b49a75290115bf36ce65326fc0fa5b8
24HP5566 24HP5566 <24HP5566@admins-iMac.local>
2025-03-29 11:52:56 +0800

    order page done

--- prisma/migrations/20250329034851_order/migration.sql
+++ prisma/migrations/20250329034851_order/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "OrderState" AS ENUM ('PENDING', 'DELIVERED', 'CANCELLED');
+
 -- CreateTable
 CREATE TABLE "Users" (
     "id" TEXT NOT NULL,
@@ -9,7 +12,6 @@ CREATE TABLE "Users" (
     "password" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "orderId" TEXT NOT NULL,
 
     CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
@@ -21,8 +23,7 @@ CREATE TABLE "Products" (
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "imgUrl" TEXT NOT NULL,
-    "categoryId" TEXT NOT NULL,
-    "orderItemsId" TEXT NOT NULL,
+    "categoryId" TEXT,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -43,9 +44,10 @@ CREATE TABLE "Category" (
 -- CreateTable
 CREATE TABLE "Order" (
     "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "orderAddress" TEXT NOT NULL,
-    "currentState" TEXT NOT NULL,
-    "usersId" TEXT NOT NULL,
+    "totalPrice" INTEGER NOT NULL,
+    "currentState" "OrderState" NOT NULL DEFAULT 'PENDING',
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -56,9 +58,10 @@ CREATE TABLE "Order" (
 CREATE TABLE "OrderItems" (
     "id" TEXT NOT NULL,
     "orderId" TEXT NOT NULL,
-    "productsId" TEXT NOT NULL,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "productId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
 
     CONSTRAINT "OrderItems_pkey" PRIMARY KEY ("id")
 );
@@ -67,16 +70,16 @@ CREATE TABLE "OrderItems" (
 CREATE UNIQUE INDEX "Users_phoneNumber_key" ON "Users"("phoneNumber");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "OrderItems_orderId_key" ON "OrderItems"("orderId");
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- AddForeignKey
-ALTER TABLE "Products" ADD CONSTRAINT "Products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Products" ADD CONSTRAINT "Products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Products" ADD CONSTRAINT "Products_orderItemsId_fkey" FOREIGN KEY ("orderItemsId") REFERENCES "OrderItems"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_usersId_fkey" FOREIGN KEY ("usersId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OrderItems" ADD CONSTRAINT "OrderItems_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItems" ADD CONSTRAINT "OrderItems_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
