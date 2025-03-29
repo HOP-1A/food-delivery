42c769664bf5067c021b773cef1dba466f5a0600
24HP4937 24HP4937 <24HP4937@ADMINs-iMac.local>
2025-03-29 12:57:33 +0800

    header zassan

--- src/app/_components/BurgerKingHeader.tsx
+++ src/app/_components/BurgerKingHeader.tsx
@@ -19,21 +19,22 @@ const Header = () => {
     HomePageItemComp;
   };
   return (
-    <div className="w-screen flex  items-center px-4 bg-[rgb(245,235,220)] mb-15 drop-shadow-lg">
-      <div className="flex gap-4 items-center mr-[720px]">
+    <div className="w-screen flex  items-center px-4  bg-[rgb(245,235,220)] mb-15 drop-shadow-lg">
+      <div className="flex gap-4 items-center mr-[610px]">
         <Menu className="text-brown-700 w-6 h-6" />
-        <div className="flex gap-2 bg-[#4D2C1D] text-white px-4 py-2 rounded-full items-center">
+        <div className="flex gap-2 bg-[#4D2C1D] text-white rounded-full items-center justify-center p-[11px] h-[50px] w-[150px]">
           <SignedOut>
             <SignInButton />
+            <div>|</div>
             <SignUpButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
         </div>
-        <div className="flex items-center border border-brown-700 rounded-full px-2 py-0.5 text-xs bg-white">
+        <div className="flex items-center border  rounded-full  text-xs bg-white h-[50px] w-[240px] border-[rgb(80,35,20)] justify-center ">
           <div className="flex items-center gap-1">
-            <Truck className="w-3.5 h-3.5 text-brown-700" />
+            <Truck className="w-5 h-5 text-brown-700" />
             <div>
               <div className="text-red-700 font-semibold leading-tight">
                 Хүргэлтийн хаяг
@@ -43,7 +44,7 @@ const Header = () => {
               </div>
             </div>
           </div>
-          <div className="border-l border-brown-700 h-3 mx-1.5"></div>
+          <div className=" border-brown-700  mx-1.5">|</div>
           <button className="flex items-center gap-1 text-brown-700 cursor-pointer">
             <Pencil className="w-3.5 h-3.5" />
             <span className="font-medium leading-tight">Өөрчлөх</span>
@@ -60,7 +61,7 @@ const Header = () => {
         <button
           onChange={HomePageItem}
           onClick={() => router.push("/cart")}
-          className="flex items-center gap-3 bg-red-700 text-white px-4 py-1 rounded-full text-base cursor-pointer"
+          className="flex items-center gap-3 bg-red-700 text-white px-4 py-2 rounded-full text-base cursor-pointer"
         >
           <ShoppingBag className="w-7 h-7 text-white" />
           <span className="font-medium">Сагс</span>
